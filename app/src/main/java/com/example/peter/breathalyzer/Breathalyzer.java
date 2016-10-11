0b6a37adffa20c64d0eed898f96183494727330b
Peter Lu <peterlu6@stanford.edu>
2016-10-10 18:53:58 -0700

    Add new activities

---
+++ app/src/main/java/com/example/peter/breathalyzer/Breathalyzer.java
@@ -0,0 +1,13 @@
+package com.example.peter.breathalyzer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Breathalyzer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breathalyzer);
+    }
+}
