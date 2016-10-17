c9782a7369397e521c8321c586033a431d6d8d02
mayala3 <mayala3@stanford.edu>
2016-10-17 11:08:46 -0700

    Added Basic Calculator Activity. Takes in 3 inputs that are not used at all later on. Performs error checking (rejects empty inputs)

--- app/src/main/java/com/example/peter/breathalyzer/Calculator.java
+++ app/src/main/java/com/example/peter/breathalyzer/Calculator.java
@@ -1,9 +1,11 @@
 package com.example.peter.breathalyzer;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Calculator extends AppCompatActivity {
 
@@ -14,6 +16,30 @@ public class Calculator extends AppCompatActivity {
     }
     public void calculate(View view){
         Intent intent = new Intent(this, Result.class);
-        startActivity(intent);
+        if(complete_inputs(view)) {
+           startActivity(intent);
+        }
+    }
+
+    //error checking - rejects input if at least one field is empty. Sets empty fields to red.
+    public boolean complete_inputs(View view){
+        EditText weight = (EditText) findViewById(R.id.weight_text);
+        EditText height = (EditText) findViewById(R.id.height_text);
+        EditText shot = (EditText) findViewById(R.id.shot_text);
+        if(input_incomplete(weight) || input_incomplete(height) || input_incomplete(shot)) {
+            weight.setHintTextColor(Color.RED);
+            height.setHintTextColor(Color.RED);
+            shot.setHintTextColor(Color.RED);
+            return false;
+        }
+        return true;
+    }
+
+    // checks if one EditText is empty
+    public boolean input_incomplete(EditText text){
+        if(text.getText().toString().trim().length() == 0){
+            return true;
+        }
+        return false;
     }
 }
