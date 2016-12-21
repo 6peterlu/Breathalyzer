b77c3d1ed6330aef198e8def71b2f00f1c0c8865
jlee29 <jlee29@stanford.edu>
2016-12-20 18:21:05 -0600

    Basic framework working (I think) some unnecessary files (welcomefrag, some XMLs)

--- app/src/main/java/com/example/peter/breathalyzer/BreathalyzerFragment.java
+++ app/src/main/java/com/example/peter/breathalyzer/BreathalyzerFragment.java
@@ -1,5 +1,9 @@
 package com.example.peter.breathalyzer;
 
+/**
+ * Created by jlee29 on 12/20/16.
+ */
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,13 +15,8 @@ import android.widget.Button;
 
 import static android.content.ContentValues.TAG;
 
-/**
- * Created by Peter on 10/20/2016.
- */
-
-public class InfoFragment extends Fragment {
-    private Button calculatorButton;
-    private Button breathButton;
+public class BreathalyzerFragment extends Fragment {
+    private Button breathalyzerButton;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -26,24 +25,14 @@ public class InfoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_welcome, container, false);
-        calculatorButton = (Button) v.findViewById(R.id.next_button);
-        calculatorButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(getActivity(), Calculator.class);
-                startActivity(intent);
-            }
-        });
-
-        breathButton = (Button) v.findViewById(R.id.breath_button);
-        breathButton.setOnClickListener(new View.OnClickListener(){
+        breathalyzerButton = (Button) v.findViewById(R.id.breath_button);
+        breathalyzerButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(getActivity(), Breathalyzer.class);
                 startActivity(intent);
             }
         });
-
         return v;
     }
 }
