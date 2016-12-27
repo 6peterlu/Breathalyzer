b6b97547ae0e1f9157a5b2cd9386618097b544b1
Peter Lu <peterlu6@stanford.edu>
2016-12-26 16:49:48 -0800

    styled info fragment

--- app/src/main/java/com/example/peter/breathalyzer/MainActivity.java
+++ app/src/main/java/com/example/peter/breathalyzer/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.peter.breathalyzer;
 //Violent delights come to violent ends
 
-import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
